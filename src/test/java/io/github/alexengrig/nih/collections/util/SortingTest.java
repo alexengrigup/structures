bbd7fff2a83d65815d2e4a01f73f538fd313e496
Grig Alex <alexengrigdev@gmail.com>
2019-09-01 21:14:42 +0300

    Refactor insertion sorting

--- src/test/java/io/github/alexengrig/nih/collections/util/SortingTest.java
+++ src/test/java/io/github/alexengrig/nih/collections/util/SortingTest.java
@@ -5,10 +5,31 @@ import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
 public class SortingTest {
+    @Test
+    public void checkInsertionForEmpty() {
+        int[] ints = {};
+        Sorting.insertion(ints);
+        assertArrayEquals(new int[]{}, ints);
+    }
 
     @Test
-    public void checkInsertion() {
-        assertArrayEquals(new int[]{1, 2, 3}, Sorting.insertion(new int[]{3, 1, 2}));
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, Sorting.insertion(new int[]{3, 1, 2, 4, 5}));
+    public void checkInsertionForSingleton() {
+        int[] ints = {1};
+        Sorting.insertion(ints);
+        assertArrayEquals(new int[]{1}, ints);
+    }
+
+    @Test
+    public void checkInsertionForDouble() {
+        int[] ints = {2, 1};
+        Sorting.insertion(ints);
+        assertArrayEquals(new int[]{1, 2}, ints);
+    }
+
+    @Test
+    public void checkInsertionForSimple() {
+        int[] ints = {3, 1, 2};
+        Sorting.insertion(ints);
+        assertArrayEquals(new int[]{1, 2, 3}, ints);
     }
 }
