593316cafd1d03285d5448fd1ec642a285e91894
Grig Alex <alexengrigdev@gmail.com>
2019-09-15 13:56:38 +0300

    Add bubble sort

--- src/main/java/io/github/alexengrig/nih/collections/util/Sorting.java
+++ src/main/java/io/github/alexengrig/nih/collections/util/Sorting.java
@@ -15,4 +15,21 @@ public class Sorting {
             input[left + 1] = key;
         }
     }
+
+    public static void bubble(int[] input) {
+        for (int major = 0, length = input.length; major < length - 1; major++) {
+            boolean swap = false;
+            for (int minor = 0; minor < length - major - 1; minor++) {
+                if (input[minor] > input[minor + 1]) {
+                    int buffer = input[minor];
+                    input[minor] = input[minor + 1];
+                    input[minor + 1] = buffer;
+                    swap = true;
+                }
+            }
+            if (!swap) {
+                break;
+            }
+        }
+    }
 }
