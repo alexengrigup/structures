b886475afee6537abf7c7805d293cda0b442b177
Grig Alex <alexengrigdev@gmail.com>
2020-01-12 17:03:25 +0300

    Add Hashing.java and HashingTest.java

---
+++ src/main/java/io/github/alexengrig/nih/collections/util/Hashing.java
@@ -0,0 +1,54 @@
+package io.github.alexengrig.nih.collections.util;
+
+public final class Hashing {
+    private Hashing() {
+    }
+
+    public static int hashCode(boolean value) {
+        return value ? 0 : 1;
+    }
+
+    public static int hashCode(byte value) {
+        return value;
+    }
+
+    public static int hashCode(char value) {
+        return value;
+    }
+
+    public static int hashCode(short value) {
+        return value;
+    }
+
+    public static int hashCode(int value) {
+        return value;
+    }
+
+    public static int hashCode(long value) {
+        return (int) (value ^ value >>> 32);
+    }
+
+    public static int hashCode(float value) {
+        return Float.floatToIntBits(value);
+    }
+
+    public static int hashCode(double value) {
+        return hashCode(Double.doubleToLongBits(value));
+    }
+
+    public static int hashCode(Object object) {
+        return object == null ? 0 : object.hashCode();
+    }
+
+    public static int hashCode(Object... objects) {
+        return hashCode(13, objects);
+    }
+
+    public static int hashCode(int initialResult, Object... objects) {
+        int result = initialResult;
+        for (Object object : objects) {
+            result = 31 * result + hashCode(object);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/io/github/alexengrig/nih/collections/util/HashingTest.java
@@ -0,0 +1,100 @@
+package io.github.alexengrig.nih.collections.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HashingTest {
+    @Test
+    public void check_hashCode_boolean() {
+        boolean value = Boolean.TRUE;
+        //noinspection ConstantConditions
+        int actual = Hashing.hashCode(value);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void check_hashCode_byte() {
+        byte value = Byte.MAX_VALUE;
+        int actual = Hashing.hashCode(value);
+        assertEquals(Byte.MAX_VALUE, actual);
+    }
+
+    @Test
+    public void check_hashCode_char() {
+        char value = Character.MAX_VALUE;
+        int actual = Hashing.hashCode(value);
+        assertEquals(Character.MAX_VALUE, actual);
+    }
+
+    @Test
+    public void check_hashCode_short() {
+        short value = Short.MAX_VALUE;
+        int actual = Hashing.hashCode(value);
+        assertEquals(Short.MAX_VALUE, actual);
+    }
+
+    @Test
+    public void check_hashCode_int() {
+        int value = Integer.MAX_VALUE;
+        int actual = Hashing.hashCode(value);
+        assertEquals(Integer.MAX_VALUE, actual);
+    }
+
+    @Test
+    public void check_hashCode_long() {
+        long value = Long.MAX_VALUE;
+        int actual = Hashing.hashCode(value);
+        assertEquals(Integer.MIN_VALUE, actual);
+    }
+
+    @Test
+    public void check_hashCode_float() {
+        float value = Float.MAX_VALUE;
+        int actual = Hashing.hashCode(value);
+        int expected = Float.floatToIntBits(Float.MAX_VALUE);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void check_hashCode_double() {
+        double value = Double.MAX_VALUE;
+        int actual = Hashing.hashCode(value);
+        int expected = Hashing.hashCode(Double.doubleToLongBits(Double.MAX_VALUE));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void check_hashCode_object() {
+        int actual = Hashing.hashCode(new Item(Integer.MAX_VALUE));
+        assertEquals(Integer.MAX_VALUE, actual);
+    }
+
+    @Test
+    public void check_hashCode_objects() {
+        int[] values = new int[]{0, 10, 20};
+        Object[] objects = new Item[3];
+        int expected = 13;
+        for (int i = 0, length = values.length; i < length; i++) {
+            int value = values[i];
+            objects[i] = new Item(value);
+            expected = 31 * expected + value;
+        }
+        int actual = Hashing.hashCode(objects);
+        assertEquals(387613, actual);
+    }
+
+    static class Item {
+        private final int value;
+
+        public Item(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public int hashCode() {
+            return value;
+        }
+    }
+}
