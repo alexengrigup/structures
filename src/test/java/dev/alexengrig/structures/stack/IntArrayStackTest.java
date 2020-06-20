829caa5a97ec9e60826b9bb20c1bfbe1e972d656
Grig Alex <alexengrigdev@gmail.com>
2020-06-20 23:10:35 +0300

    Stack: Create tests for IntArrayStackTest

---
+++ src/test/java/dev/alexengrig/structures/stack/IntArrayStackTest.java
@@ -0,0 +1,110 @@
+package dev.alexengrig.structures.stack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IntArrayStackTest {
+    @Test
+    public void should_create_empty() {
+        IntArrayStack stack = new IntArrayStack();
+        assertTrue(stack.empty());
+        assertEquals(0, stack.size());
+        assertEquals(8, stack.array.length);
+    }
+
+    @Test
+    public void should_create_emptyWithCustomCapacity() {
+        IntArrayStack stack = new IntArrayStack(4);
+        assertTrue(stack.empty());
+        assertEquals(0, stack.size());
+        assertEquals(4, stack.array.length);
+    }
+
+    @Test
+    public void should_push_8_elements() {
+        IntArrayStack stack = new IntArrayStack();
+        for (int i = 0; i < 8; i++) {
+            stack.push(i);
+        }
+        assertFalse(stack.empty());
+        assertEquals(8, stack.size());
+        assertEquals(8, stack.array.length);
+    }
+
+    @Test
+    public void should_push_12_elements() {
+        IntArrayStack stack = new IntArrayStack();
+        for (int i = 0; i < 12; i++) {
+            stack.push(i);
+        }
+        assertFalse(stack.empty());
+        assertEquals(12, stack.size());
+        assertEquals(16, stack.array.length);
+    }
+
+    @Test
+    public void should_push_24_elements() {
+        IntArrayStack stack = new IntArrayStack();
+        for (int i = 0; i < 24; i++) {
+            stack.push(i);
+        }
+        assertFalse(stack.empty());
+        assertEquals(24, stack.size());
+        assertEquals(32, stack.array.length);
+    }
+
+    @Test
+    public void should_pop_8_elements() {
+        IntArrayStack stack = new IntArrayStack();
+        for (int i = 0; i < 8; i++) {
+            stack.push(i);
+        }
+        for (int i = 7; i >= 0; i--) {
+            assertEquals(i, stack.pop());
+        }
+        assertTrue(stack.empty());
+        assertEquals(0, stack.size());
+        assertEquals(8, stack.array.length);
+    }
+
+    @Test
+    public void should_pop_12_elements() {
+        IntArrayStack stack = new IntArrayStack();
+        for (int i = 0; i < 12; i++) {
+            stack.push(i);
+        }
+        for (int i = 11; i >= 0; i--) {
+            assertEquals(i, stack.pop());
+        }
+        assertTrue(stack.empty());
+        assertEquals(0, stack.size());
+        assertEquals(16, stack.array.length);
+    }
+
+    @Test
+    public void should_pop_24_elements() {
+        IntArrayStack stack = new IntArrayStack();
+        for (int i = 0; i < 24; i++) {
+            stack.push(i);
+        }
+        for (int i = 23; i >= 0; i--) {
+            assertEquals(i, stack.pop());
+        }
+        assertTrue(stack.empty());
+        assertEquals(0, stack.size());
+        assertEquals(32, stack.array.length);
+    }
+
+    @Test
+    public void should_return_actualTop() {
+        IntArrayStack stack = new IntArrayStack();
+        for (int i = 0; i < 24; i++) {
+            stack.push(i);
+            assertEquals(i, stack.top());
+        }
+        assertFalse(stack.empty());
+        assertEquals(24, stack.size());
+        assertEquals(32, stack.array.length);
+    }
+}
