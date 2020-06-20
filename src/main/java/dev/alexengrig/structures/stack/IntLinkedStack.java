d59b94366dec404cd07804eca6065a6792a902b1
Grig Alex <alexengrigdev@gmail.com>
2020-06-21 01:21:07 +0300

    Stack: Refactor IntLinkedStack.Node

--- src/main/java/dev/alexengrig/structures/stack/IntLinkedStack.java
+++ src/main/java/dev/alexengrig/structures/stack/IntLinkedStack.java
@@ -40,8 +40,8 @@ public class IntLinkedStack implements IntStack {
     }
 
     protected static class Node {
-        int value;
-        Node next;
+        public int value;
+        public Node next;
 
         public Node(int value, Node next) {
             this.value = value;
