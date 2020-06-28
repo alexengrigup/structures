6d2f735ca301cf5d5ab42ac965b19a913eea75b9
Grig Alex <alexengrigdev@gmail.com>
2020-06-28 22:21:26 +0300

    Queue: Add Big O notation to BaseIntQueueWithMaxViaStack

--- src/main/java/dev/alexengrig/structures/queue/BaseIntQueueWithMaxViaStack.java
+++ src/main/java/dev/alexengrig/structures/queue/BaseIntQueueWithMaxViaStack.java
@@ -1,10 +1,12 @@
 package dev.alexengrig.structures.queue;
 
+import dev.alexengrig.structures.annotation.O;
 import dev.alexengrig.structures.stack.IntStackWithMax;
 
 public abstract class BaseIntQueueWithMaxViaStack
         extends BaseIntQueueViaStack<IntStackWithMax>
         implements IntQueueWithMax {
+    @O("1")
     @Override
     public int max() {
         requireNonEmpty();
