4e0d1814d4836a7531b5e81f5e99208d4d36f0ba
Grig Alex <alexengrigdev@gmail.com>
2020-07-16 23:04:33 +0300

    Array: Create swap* methods with tests

--- src/main/java/dev/alexengrig/structures/array/IntArray.java
+++ src/main/java/dev/alexengrig/structures/array/IntArray.java
@@ -1,6 +1,35 @@
 package dev.alexengrig.structures.array;
 
 public interface IntArray {
+    static void swap(IntArray array, int from, int to) {
+        array.set(to, array.set(from, array.at(to)));
+    }
+
+    static void swap(int[] array, int from, int to) {
+        int temp = array[from];
+        array[from] = array[to];
+        array[to] = temp;
+    }
+
+    static void swapSum(int[] array, int from, int to) {
+        // array[from] = array[from] + array[to] - (array[to] = array[from])
+        array[from] = array[from] + array[to];
+        array[to] = array[from] - array[to];
+        array[from] = array[from] - array[to];
+    }
+
+    static void swapDiff(int[] array, int from, int to) {
+        array[from] = array[from] - array[to];
+        array[to] = array[from] + array[to];
+        array[from] = array[to] - array[from];
+    }
+
+    static void swapXor(int[] array, int from, int to) {
+        array[from] = array[from] ^ array[to];
+        array[to] = array[from] ^ array[to];
+        array[from] = array[from] ^ array[to];
+    }
+
     int at(int index);
 
     int set(int index, int value);
---
+++ src/test/java/dev/alexengrig/structures/array/IntArrayTest.java
@@ -0,0 +1,35 @@
+package dev.alexengrig.structures.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class IntArrayTest {
+    @Test
+    public void should_swap_elements_in_array() {
+        int[] array = {2, 1};
+        IntArray.swap(array, 0, 1);
+        assertArrayEquals(new int[]{1, 2}, array);
+    }
+
+    @Test
+    public void should_swap_elements_in_array_via_sum() {
+        int[] array = {2, 1};
+        IntArray.swapSum(array, 0, 1);
+        assertArrayEquals(new int[]{1, 2}, array);
+    }
+
+    @Test
+    public void should_swap_elements_in_array_via_diff() {
+        int[] array = {2, 1};
+        IntArray.swapDiff(array, 0, 1);
+        assertArrayEquals(new int[]{1, 2}, array);
+    }
+
+    @Test
+    public void should_swap_elements_in_array_via_xor() {
+        int[] array = {2, 1};
+        IntArray.swapXor(array, 0, 1);
+        assertArrayEquals(new int[]{1, 2}, array);
+    }
+}
