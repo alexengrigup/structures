bf748d2d3784d7a46904b9c732f8bb94e0b3bcaf
Grig Alex <alexengrigdev@gmail.com>
2021-07-19 23:51:23 +0300

    Create IntConcurrentArray

---
+++ src/main/java/dev/alexengrig/structures/array/IntConcurrentArray.java
@@ -0,0 +1,18 @@
+package dev.alexengrig.structures.array;
+
+public class IntConcurrentArray implements IntArray {
+    @Override
+    public int at(int index) {
+        return 0;
+    }
+
+    @Override
+    public int set(int index, int value) {
+        return 0;
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+}
