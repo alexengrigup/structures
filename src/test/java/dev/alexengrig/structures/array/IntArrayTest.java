0d15399d0b13afcbe2677187e5de15d77c83c4cc
Grig Alex <alexengrigdev@gmail.com>
2021-07-24 20:41:19 +0300

    Update IntPlainArray and add tests

--- src/test/java/dev/alexengrig/structures/array/IntArrayTest.java
+++ src/test/java/dev/alexengrig/structures/array/IntArrayTest.java
@@ -3,8 +3,17 @@ package dev.alexengrig.structures.array;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class IntArrayTest {
+    @Test
+    public void should_swap_elements_in_intArray() {
+        IntArray array = IntPlainArray.from(new int[]{2, 1});
+        IntArray.swap(array, 0, 1);
+        assertEquals(1, array.at(0));
+        assertEquals(2, array.at(1));
+    }
+
     @Test
     public void should_swap_elements_in_array() {
         int[] array = {2, 1};
